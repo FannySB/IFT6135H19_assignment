1a8ee657d51dbb5f45df8815805ea0afd932cc84
Fanny <fanny.salvail-berard@usherbrooke.ca>
2019-02-09 18:32:42 -0500

    Gradient, weight initializations

--- MLP.py
+++ MLP.py
@@ -1,13 +1,16 @@
 import numpy as np
+from math import sqrt
 from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
 import _pickle as pickle
 import gzip
 
 
 class NN(object):
 
-    def __init__(self, hidden_dims=(1024, 2048), n_hidden=2, mode='train', datapath=None, model_path=None):
+    def __init__(self, hidden_dims=(8, 10), n_hidden=2, mode='train', datapath=None, model_path=None, learning_rate=0.01):
         print('Init')
+        self.learning_rate = learning_rate
         self.n_hidden = n_hidden
         h1 = hidden_dims[0]
         h2 = hidden_dims[1]
@@ -20,74 +23,110 @@ class NN(object):
 
     def verif_param_nn(self, total_param_nn):
         print('verif_param_nn')
-        assert (not (total_param_nn < 1000000 & total_param_nn > 500000),
-                'ERROR! Number of parameter in NN: ' + str(total_param_nn))
+        if not (total_param_nn < 1000000 and total_param_nn > 500000):
+            raise AssertionError('ERROR! Number of parameter in NN: ' + str(total_param_nn))
 
-    # %%
     def initialize_weights(self, method='Normal'):
         print('initialize_weights')
         weights = {}
         bias = {}
+
         if method == 'Normal':
-            for layer in range(len(self.dims) - 1):
-                weights[layer] = np.random.normal(0, 0.05, size=(self.dims[layer], self.dims[layer + 1]))
-                bias[layer] = np.random.normal(0, 0.05, size=self.dims[layer + 1])
+            for layer in range(1, len(self.dims)):
+                weights[layer - 1] = np.random.randn(self.dims[layer - 1], self.dims[layer]) * np.sqrt(2 / self.dims[layer - 1])
+                bias[layer - 1] = np.zeros((self.dims[layer],), dtype=float)
         elif method == 'Zero':
-            for layer in range(len(self.dims) - 1):
-                line = np.zeros((self.dims[layer], self.dims[layer + 1]), dtype=float)
-                weights[layer] = line
-                bias[layer] = np.zeros((self.dims[layer + 1],), dtype=float)
+            for layer in range(1, len(self.dims)):
+                line = np.zeros((self.dims[layer -1], self.dims[layer]), dtype=float)
+                weights[layer - 1] = line
+                bias[layer - 1] = np.zeros((self.dims[layer],), dtype=float)
         else:
-            for layer in range(len(self.dims) - 1):
-                weights[layer] = np.random.rand(self.dims[layer], self.dims[layer + 1])
-                bias[layer] = np.random.rand(self.dims[layer + 1])
+            for layer in range(1, len(self.dims)):
+                h_layer = self.dims[layer]
+                h_last_layer = self.dims[layer - 1]
+                d_layer = sqrt(6/ (h_last_layer + h_layer))
+
+                line = np.random.uniform(-d_layer, d_layer)
+                weights[layer - 1] = line
+                bias[layer - 1] = np.zeros((self.dims[layer],), dtype=float)
+
         return weights, bias
 
-    # %%
     def forward(self, input, weights, bias):
-        print('forward')
         h1 = self.activation(np.dot(input, weights[0]) + bias[0])
         h2 = self.activation(np.dot(h1, weights[1]) + bias[1])
         out = self.softmax(np.dot(h2, weights[2]) + bias[2])
-        print(h1.shape, h2.shape, out.shape)
         return out
 
-    # %%
     def activation(self, input):
+        """
+        Relu
+        :param input:
+        :return:
+        """
         return np.maximum(0, input)
 
-    # %%
     def loss(self, prediction, labels):
-        print('loss')
+        """
+        Cross entropy
+        :param prediction:
+        :param labels:
+        :return:
+        """
+        # print('loss')
         pred = np.max(prediction, axis=0)
         return -np.sum(labels * np.log(pred))
 
-    # %%
     def softmax(self, input):
-        print('softmax')
+        # print('softmax')
         input -= np.max(input)
         #print('Values softmax:', np.exp(input) / np.sum(np.exp(input)))
         return np.exp(input) / np.sum(np.exp(input))
 
-    # %%
-    def backward(self, cache, labels):
+    def backward(self, cache, labels, input, weights, bias):
         print('backward')
+        # Relu derivative
+        relu_d =  1 * (input > 0)
+
+        length = input.shape[0]
         # SGD
 
-    # %%
-    def update(self, grads):
+    def update(self, grads_w, grads_b, weight, bias):
+        """
+        SDG
+        :param grads:
+        :return:
+        """
+        weight -= np.multiply(learning_rate, grads_w)
+        bias -= np.multiply(learning_rate, grads_b)
+
+        return weight, bias
+
         print('update')
 
-    # %%
     def train(self, input, labels):
         # print('train')
         # predicted = self.forward()
         # self.loss(predicted, labels)
         return input
 
-    # %%
-    def test(self):
+    def test(self, weights, prediction, label):
         print('test')
+        N_vector = [1, 10, 1000, 125000, 6250000]
+        for N in N_vector:
+            epsilon = 1/N
+            grads = []
+            p = min(10, len(weights))
+            target_weights = weights[:p]
+            target_labels = labels[:p]
+            for i in range(p):
+                mask = np.zeros(p)
+                mask[i] = epsilon
+                grads.append((mlp.loss(np.sum(target_weights, mask)) - mlp.loss(np.diff(weights, mask)))/ 2*epsilon)
+
+            plt.plot(grads)
+            plt.savefig('test_grads' + str(N) + '.png')
+
 
 
 def normalize(a, axis=-1, order=2):
@@ -113,24 +152,38 @@ validation_labels = mnist[1, 1]
 test = mnist[2, 0]
 test_labels = mnist[2, 1]
 
+
+
+
 mlp = NN()
 mlp.verif_param_nn(mlp.total_param_nn)
+# weights, bias = mlp.initialize_weights(method='Zero')
 weights, bias = mlp.initialize_weights(method='Normal')
-predicted = mlp.forward(train_sample, weights, bias)
-loss = mlp.loss(predicted, train_labels_sample)
+# weights, bias = mlp.initialize_weights(method='Glorot')
+epochs = 10
+learning_rate = 0.01
+mean_error = []
+for epoch in range(epochs):
+    cum_error = 0
+    for idx in range(len(train_sample)):
+        data = train_sample[idx]
+        labels = train_labels_sample[idx]
 
+        predicted = mlp.forward(data, weights, bias)
+        error = mlp.loss(predicted, labels)
 
-# epochs = 1
-# learning_rate = 0.01
-# for epoch in range(epochs):
-#     cum_loss = 0
-#     for idx in range(len(train_sample)):
-#         data = train_sample[idx]
-#         labels = train_labels_sample[idx]
-#
-#         predicted = mlp.forward(data, weights, bias)
-#         ce = mlp.loss(predicted, labels)
-#
-#         print("predicted = ", predicted)
-#         print("loss ", ce )
-#         print(ce)
+        #backprop missing
+        # weights, bias = mlp.update(grads_w, grads_b, weights, bias)
+
+        cum_error += error
+        # print("predicted = ", predicted)
+        # print("loss ", idx_loss)
+    mean_error.append(cum_error/len(train_sample))
+    print('epoch:', epoch, 'mean_error:', mean_error[epoch])
+plt.plot(mean_error)
+plt.savefig('mean_error.png')
+
+# Test on the 2nd hidden layer by homework's instructions
+predictions = mlp.forward(data, weights, bias)
+mlp.test(weights[1], predictions[0], labels[0])
+
